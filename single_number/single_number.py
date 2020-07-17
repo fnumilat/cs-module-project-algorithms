ed65d3d55f962fb163ed88190a3c5691a12dfe5e
unknown <fnumilat@gmail.com>
2020-07-16 19:29:01 -0500

    single_number optimized

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,21 +2,28 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# def single_number(arr):
+#     # Your code here
+#     # create an array to hold the single number
+#     single = []
+#     # iterate through numbers
+#     for target in arr:
+#         # check to see if the target is already in the created array
+#         if target not in single:
+#             single.append(target)
+#         # if it is, remove it from array
+#         else:
+#             single.remove(target)
+#     # when iteration is done, the only number in the array is the odd number out
+#     # return odd number
+#     return single[0]
+
+# Better Solution
 def single_number(arr):
-    # Your code here
-    # create an array to hold the single number
-    single = []
-    # iterate through numbers
-    for target in arr:
-        # check to see if the target is already in the created array
-        if target not in single:
-            single.append(target)
-        # if it is, remove it from array
-        else:
-            single.remove(target)
-    # when iteration is done, the only number in the array is the odd number out
-    # return odd number
-    return single[0]
+    arr.sort()
+    for i in range(0, len(arr), 2):
+        if arr[i] + arr[i+1] - arr[i] != arr[i]:
+            return arr[i]
 
    
 
