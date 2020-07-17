40707dc9a7c15189d073be7dc517f2b672abd5ca
unknown <fnumilat@gmail.com>
2020-07-16 21:24:52 -0500

    moving_zeroes optimized

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,23 +2,38 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# First Pass Solution
+# def moving_zeroes(arr):
+#     # Your code here
+#     # create two arrays, (left and right) to hold the numbers when they are separated
+#     left = []
+#     right = []
+#     # iterate through the array of the numbers and 
+#     # see if the number is equal to zero then add it to the right array
+#     # and if not add it to the left array
+#     for num in arr:
+#         if num == 0:
+#             right.append(num)
+#             print(left, "left", right, "right")
+#         else:
+#             left.append(num)
+#             print(left, "left", right, "right")
+#     # return both (left and right) arrays merged
+#     return left + right
+
+# Better Solution
 def moving_zeroes(arr):
-    # Your code here
-    # create two arrays, (left and right) to hold the numbers when they are separated
-    left = []
-    right = []
-    # iterate through the array of the numbers and 
-    # see if the number is equal to zero then add it to the right array
-    # and if not add it to the left array
-    for num in arr:
-        if num == 0:
-            right.append(num)
-            print(left, "left", right, "right")
+    start = 0
+    end = len(arr)-1
+    for _i in range(end):
+        if arr[start] == 0:
+            arr.append(0)
+            del arr[start]
+            end -=1
         else:
-            left.append(num)
-            print(left, "left", right, "right")
-    # return both (left and right) arrays merged
-    return left + right
+            start+=1
+    
+    return arr
 
     
 
